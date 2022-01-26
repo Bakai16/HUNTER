c99200fdb1dcafa8a5b5feebb2b8e3dcf4372c69
Bakai Akylbekov <bakayakylbekov3@gmail.com>
2022-01-26 21:29:36 +0600

    first commit

---
+++ # js fitst 1 /script.js
@@ -0,0 +1,138 @@
+ "use sctrict";
+
+// // a = 15;
+// // console.log(a);
+
+
+// let number = 5;
+
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+
+
+// // const obj ={
+// //     a: 50
+// // };
+// // obj.a = 10;
+
+// // console.lob (obj);
+
+
+// console.log(name);
+// var name = 'Bakai';
+
+// {
+//     var result = 50;
+// }
+// console.log(result);
+
+
+// //js 2 lesson
+
+// let nunber = 4.5;
+
+// console.log(-4/0);
+// console.log('string'*9);
+
+
+
+// const person = `5`;
+
+// const bool = false;
+
+
+// // console.log(something);
+
+
+// let und;
+// console.log(und);
+
+
+// const obj ={
+//     name: 'John',
+//     age: 25,
+//     isMarried: false
+// };
+// // console.log(obj.name);
+// console.log(obj['name']);
+
+
+
+// //Массивы
+
+// let arr = ['plum.png', 'orange.jpg',6,  'apple.bmp' , {}, []];
+// console.log(arr[0]);
+
+
+
+//7-lesson простое общение с пользователем
+
+// alert('Hello World!');
+
+// const result = confirm('Are you here?');
+// console.log(result);
+
+// const answer = + prompt ("Вам есть 18?", "18");
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваше Фамиля?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+// document.write(answers);
+
+
+
+//8-lesson  интерполяция (ES6)
+// const category = 'toys';
+// console.log(`https://someurl.com/${category}/5`);
+
+// const uer = 'Bakai';
+// alert(`Привет, мир! Меня зовут ${uer}`);
+
+
+
+//9-lesson  операторы в JS
+console.lo('arr' + ' - object');
+console.log(4 + '5');
+
+
+//инкремент декремент көбөйтүү азайтуу
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+console.log(2 * 4 + 2  !=8);
+console.log(2 + 2 *2  !== '6');
+
+// &&   и
+// || или
+
+const isChecked = false,
+        isClose = false;
+console.log(isChecked || !isClose);
+
+
+// a= "5";
+// b =  2;
+// c =  a+++b;
+
+// switch (""){
+//     case "b":
+//         console.log("b"); break;
+//         default 
+//         console.log("d");
+        
+// }
+
---
+++ Условия -13 /script.js
@@ -0,0 +1,48 @@
+
+
+if ( 4 == 4){
+    console.log("Ok!");
+} else{
+    console.log("Error!");
+}
+
+
+
+
+
+// const num = 50;
+
+// if (num < 49){
+//     console.log("Error!");
+// } else if ( num > 100)
+//     console.lgo("Много");
+// else {
+//     console.log("Ok!");
+// }
+
+// // Тернарного оператора   ? :   единственный оператор
+
+// (num ===50) ? console.log ("Ok!") : console.log("Error!");
+
+//бинарный оргумуент
+//  
+
+//унарный оргумуент
+// +'4';
+
+const num = 50;
+
+switch (num) {
+    case 49:
+        console.log('Неверно');
+        break;
+    case 100:
+        console.log('Неверно');
+        break;
+    case 52: 
+        console.log('В точку');
+        break;
+    default:
+        console.log('Не в этот раз');
+        break;
+}
