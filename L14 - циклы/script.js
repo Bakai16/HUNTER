c328bbc9ce6601b97b56067a3792ca0931f999d4
Bakai Akylbekov <bakayakylbekov3@gmail.com>
2022-01-31 18:41:40 +0600

    Lesson 14 Циклы

--- L14 - циклы/script.js
+++ L14 - циклы/script.js
@@ -1,6 +1,37 @@
-let num = 50;
+// let num = 50;
+// while (num <= 55 ){
+//     console.log(num);
+//     num++;
+// }
+//50      expected outputo
+//51
+//52
+//53
+//54
+//55
 
-while (num < 55 ){
-    alert(num);
+//второй цикл
+// let num = 50;
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while (num <= 55);
+// //expected output 50 51 52 53 54 55
+
+let num = 50;
+for (let i = 1; i < 8; i++){
+    console.log(num);
     num++;
+}
+// expected output 50 51 52 53 54 55 56 
+
+
+//break и continue
+for (let i = 1; i < 10; i++){
+    if (i === 6){
+        // break;   ///expected output 1 2 3 4 5
+        continue;   ///expected output 1 2 3 4 5 7 8 9
+    }
+    console.log(i);
 }
