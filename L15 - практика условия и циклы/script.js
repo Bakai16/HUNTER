84ef03254cb18bea441541850192744b8f0dc593
Bakai Akylbekov <bakayakylbekov3@gmail.com>
2022-02-01 23:16:52 +0600

    lesson 15 completed

--- L15 - практика условия и циклы/script.js
+++ L15 - практика условия и циклы/script.js
@@ -34,4 +34,13 @@ for (i = 0; i < 2; i++) {
     }
 
 }
-console.log(personalMovieDB);
+if (personalMovieDB.count < 10){
+    console.log('Просмотрено довольно мало фильмов');
+}else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30 ){
+    console.log('Вы классический зритель');
+}else if (personalMovieDB.count >= 30){
+    console.log('Вы киноман');
+}else {
+    console.log("Произошла ошибка")
+}
+   
