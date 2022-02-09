b93f0a51db99aa407bb92ceba63444981e5ee523
Bakai Akylbekov <bakayakylbekov3@gmail.com>
2022-02-09 06:04:35 +0600

    Lesson 17 методы и свойства строк и чисел

---
+++ L17 - методы и свойства строк и чисел/script17.js
@@ -0,0 +1,38 @@
+const str = "Test";
+const arr = [1, 2, 3];
+
+console.log(arr.length);
+// console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+//expected output: 4
+//expected output: 3
+
+
+
+const fruit = "Some  fruit";
+
+console.log(fruit.indexOf("fruit"));
+//expected output: 6
+
+
+const logg = "Hello world";
+console.log(logg.slice(6, 10));
+//expected output: worl
+console.log(logg.slice(-5, -1));
+//expected output: worl
+console.log(logg.substring(6, 11)); //
+//expected output: world
+console.log(logg.substr(6, 5));
+//expected output: world
+
+
+const num = "12.2";
+console.log(Math.round(num));
+//expected output: 12
+
+
+const num1 = "12.2px";
+console.log(parseInt(num2));
+//expected output: 12
+
+
