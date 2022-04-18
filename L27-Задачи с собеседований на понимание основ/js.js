bee64cd971aedb13ef9b1d27426f30ac8cb461f8
Akylbekov <user@Users-MacBook-Pro.local>
2022-04-18 16:27:11 +0600

    L27-Задачи с собеседований на понимание основ

---
+++ L27-Задачи с собеседований на понимание основ/js.js
@@ -0,0 +1,40 @@
+// let x = 5; alert( ++x );    //6
+
+// [ ] + false - null + true
+// console.log([] + false - null + true);   //NaN
+
+// let y = 1; 
+// let x = y = 2;
+// alert(x);                //2
+
+
+// console.log([ ] + 1 + 2);      //"12"
+
+
+// alert( "1"[0] );      //"1"
+
+
+// console.log(2 && 1 && null && 0 && undefined);   //null
+// И запинается на лжи   Жана жалганга чалынат
+// ИЛИ запинается на правди
+ 
+
+// console.log(!!( 1 && 2 ) ===  (1 && 2));     //false boolen овые значение !!
+
+// ИЛИ запинается на правди
+// alert( null || 2 && 3 || 4 );     ///"3"
+
+
+// const a = [1, 2, 3];
+// const b = [1, 2, 3]; 
+
+// console.log(a == b);   //false
+
+
+// alert( +"Infinity" );   ///унарный +   infinity
+
+
+// console.log("Ёжик" > "яблоко");   //false
+
+
+console.log(0 || "" || 2 || undefined || true || falsе);   //"2"
