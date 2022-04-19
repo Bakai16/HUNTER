579cd6b572bed304a95e8124167e01d2648df051
Akylbekov <user@Users-MacBook-Pro.local>
2022-04-19 15:10:55 +0600

    события button click

---
+++ L31-События и их обработчики onclick/script.js
@@ -0,0 +1,51 @@
+const btns = document.querySelectorAll('button');
+     overlay = document.querySelector('.overlay');
+
+// btn.onclick = function(){
+//     alert('click');
+// };
+
+// btn.onclick = function(){
+//     alert('Second click');
+// };
+
+// btn.addEventListener('mouseenter', (e) =>{
+//     console.log(e.target);
+//     e.target.remove();
+//     // console.log("hover");
+// });
+
+// btn.addEventListener('click', () =>{
+//     alert('Second click');
+// })
+
+
+// let i = 0;
+const deleteElement = (e) =>{
+    console.log(e.target);
+    console.log(e.type);
+    // i++;
+    // if( i == 1){
+    //     btn.removeEventListener('click', deleteElement);
+    // }
+};
+
+
+// btn.addEventListener('click', deleteElement);
+// overlay.addEventListener('click', deleteElement);
+
+btns.forEach(btn =>{
+    btn.addEventListener('click' , deleteElement,  {once: true});   //option true once 
+});
+
+
+
+///a
+
+const link = document.querySelector('a');
+
+link.addEventListener('click', function(event) {
+    event.preventDefault();
+
+    console.log(event.target);
+});
