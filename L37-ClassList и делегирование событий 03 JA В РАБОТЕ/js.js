c0cbdf3aa1c8f82ec27ca0d126763b79bcbb4a9d
Akylbekov <user@Users-MacBook-Pro.local>
2022-04-21 17:37:43 +0600

    ClassList

---
+++ L37-ClassList и делегирование событий 03 JA В РАБОТЕ/js.js
@@ -0,0 +1,46 @@
+const btns = document.querySelectorAll('button');
+      wrapper = document.querySelector('.btn-block');
+
+// console.log(btns[0].classList.length);
+// console.log(btns[0].classList.item(1));   //some   class
+// console.log(btns[1].classList.add('red', 'djfdjfjjfjs'));  //добавить 
+// console.log(btns[0].classList.remove('blue'));   //remove blut class
+// console.log(btns[0].classList.toggle('blue'));   //кайтып келди blut class
+
+
+// if(btns[1].classList.contains('red')){
+//     console.log('red');
+// }
+
+btns[0].addEventListener('click', () => {
+    // if(!btns[1].classList.contains('red')){
+    //     btns[1].classList.add('red');
+    // }
+    // else{
+    //     btns[1].classList.remove('red');
+    // }
+
+    btns[1].classList.toggle('red');
+});
+
+
+// console.log(btns[0].className);
+
+wrapper.addEventListener('click', (event) => {
+    // if(event.target && event.target.classList.contains('blue')){
+    // if(event.target && event.target.tagName == 'BUTTON'){
+    if(event.target && event.target.matches('button.red')){
+        console.log("hello");
+    }
+});
+
+// btns.forEach(btn => {
+//     btn.addEventListener('click', () =>{
+//         console.log("hello");
+//     });
+// });
+
+const btn = document.createElement('button');
+btn.classList.add("red");
+wrapper.append(btn);
+
