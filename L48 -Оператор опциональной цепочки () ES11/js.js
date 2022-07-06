addef3f2a1286f127100f12248218cc9a31c309f
Akylbekov <user@Users-MacBook-Pro.local>
2022-07-06 15:56:34 +0600

    (д) Оператор опциональной цепочки (?.) ES11

---
+++ L48 -Оператор опциональной цепочки () ES11/js.js
@@ -0,0 +1,15 @@
+const box = document.querySelector('.box');
+const block = document.querySelector('.block');
+ 
+
+const userData = {
+  name: 'Ivan',
+  age: null,
+  say: function(){
+    console.log("Hello");
+  }
+}
+userData.say();
+userData.hey?.();
+
+
