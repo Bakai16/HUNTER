586e27f3e0318ffc36d5b537e1a606148b559f7b
Akylbekov <user@Users-MacBook-Pro.local>
2022-10-10 16:37:05 +0600

    AJAX и общение с сервером

---
+++ L77- calc AJAX и общение с сервером/js/script.js
@@ -0,0 +1,26 @@
+
+const inputRub = document.querySelector('#rub'),
+      inputUsd = document.querySelector('#usd');
+
+inputRub.addEventListener('input', ()=>{
+  const request = new XMLHttpRequest();
+
+  // request.open(methot, url, async, login, pass);
+  request.open('GET', 'js/current.json');
+  request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+  request.send();
+
+  request.addEventListener('load', ()=>{
+    if (request.status === 200){ 
+      const data = JSON.parse(request.response);
+      inputUsd.value = (+inputRub.value / data.current.usd).toFixed(2);
+    }else{
+      inputUsd.value = "что-то пошло не так";
+    }
+  });
+
+  //status
+  //statusText
+  //response 
+  //readyState
+});       
