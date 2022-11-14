18e2776c0fb5063f532c111ac180b0a28be3fc97
Akylbekov <user@Users-MacBook-Pro.local>
2022-11-14 19:33:52 +0600

    animate()

---
+++ L106- JS анимации, requestAnimationFrame/script.js
@@ -0,0 +1,36 @@
+const btn = document.querySelector('.btn');
+const elem = document.querySelector('.box');
+let timerId;
+i = 0;
+let pos = 0;
+
+// function myAnimation(){
+//     const elem = document.querySelector('.box');
+//     let pos = 0;
+
+//     const id = setInterval(frame, 10);
+//     function frame() {
+//         if (pos == 300) {
+//             clearInterval(id);
+//         } else {
+//             pos++;
+//             elem.style.top = pos + "px";
+//             elem.style.left = pos + "px";
+//         }
+//     }
+
+function myAnimation(){
+  pos++;
+  elem.style.top = pos + "px";
+  elem.style.left = pos + "px";
+  
+  if(pos < 300){
+    requestAnimationFrame(myAnimation);
+  }
+
+}
+
+btn.addEventListener('click', ()=> requestAnimationFrame(myAnimation));
+
+const id = requestAnimationFrame(myAnimation);
+cancelAnimationFrame(id);
